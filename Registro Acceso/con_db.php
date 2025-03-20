0d05c2e3a331a65caf2ee66ce0f4641ccc3c8647
Nvelez1601 <nvelezcuauro@gmail.com>
2025-03-19 21:40:37 -0400

    Reinicio
    Reinicio y Limpieza

--- Registro Acceso/con_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// conxion con la base de datos
-$conexion = mysqli_connect("localhost", "root", "", "acceso_db");
-
-if (!$conexion) {
-    die("Error de conexión: " . mysqli_connect_error());
-}
-?>
